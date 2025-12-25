fd89d0bf534205a3748881f491472ba3967e7799
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-25 11:16:18 -0500

    trim data lookups for network vpc defaults

--- terraform/account_layer/data.tf
+++ terraform/account_layer/data.tf
@@ -18,22 +18,6 @@ data "aws_iam_policy" "read_only_access" {
 
 data "aws_region" "current" {}
 
-data "aws_subnet" "default" {
-  for_each = toset(data.aws_subnets.default.ids)
-  id       = each.value
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
 data "local_file" "pgp_key" {
   filename = abspath("./public_key_binary_base64encoded.gpg")
 }
