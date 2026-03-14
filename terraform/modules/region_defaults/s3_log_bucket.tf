d5455a8d2f89a3634002817a95b1b2061510fdbd
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-03-14 17:12:30 -0400

    add cloudtrail trail to region defaults module

--- terraform/modules/region_defaults/s3_log_bucket.tf
+++ terraform/modules/region_defaults/s3_log_bucket.tf
@@ -73,6 +73,7 @@ resource "aws_s3_bucket_ownership_controls" "logs" {
 data "aws_iam_policy_document" "logs" {
   source_policy_documents = [
     data.aws_iam_policy_document.default_vpc_flow_logs.json,
+    data.aws_iam_policy_document.cloudtrail.json,
   ]
   override_policy_documents = []
 
