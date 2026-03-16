d0f5b8fecd0cf18a4be56f0a8a1da406a616703d
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-03-16 17:39:07 -0400

    adjust naming

--- terraform/modules/cloudtrail/main.tf
+++ terraform/modules/cloudtrail/main.tf
@@ -3,19 +3,19 @@ locals {
   aws_region          = data.aws_region.current.region
   aws_regions_enabled = data.aws_regions.enabled.names
 
-  cloudtrail_athena_table_name = "cloudtrail_multi_region_new" # should be lowercase letters, numbers, or underscore
-  cloudtrail_name              = "new-multi-region-trail-${local.aws_account_id}-${local.aws_region}"
+  cloudtrail_athena_table_name = "cloudtrail_multi_region" # should be lowercase letters, numbers, or underscore
+  cloudtrail_name              = "multi-region-trail-${local.aws_account_id}-${local.aws_region}"
   cloudtrail_s3_key_prefix     = "cloudtrail"
 
   cloudwatch_log_group_name                                   = "/aws/cloudtrail/${local.cloudtrail_name}"
-  cloudwatch_log_metric_filter_name_console_login_without_mfa = "console-login-without-mfa-new"
-  cloudwatch_log_metric_filter_name_root_user                 = "root-user-activity-new"
-  cloudwatch_metric_alarm_name_console_login_without_mfa      = "console-login-without-mfa-new"
-  cloudwatch_metric_alarm_name_root_user                      = "root-user-activity-new"
+  cloudwatch_log_metric_filter_name_console_login_without_mfa = "console-login-without-mfa"
+  cloudwatch_log_metric_filter_name_root_user                 = "root-user-activity"
+  cloudwatch_metric_alarm_name_console_login_without_mfa      = "console-login-without-mfa"
+  cloudwatch_metric_alarm_name_root_user                      = "root-user-activity"
 
-  iam_policy_name_cloudtrail_to_cloudwatch = "new-cloudtrail-to-cloudwatch"
-  iam_role_name_cloudtrail_to_cloudwatch   = "new-cloudtrail-to-cloudwatch"
+  iam_policy_name_cloudtrail_to_cloudwatch = "cloudtrail-to-cloudwatch"
+  iam_role_name_cloudtrail_to_cloudwatch   = "cloudtrail-to-cloudwatch"
 
-  sns_topic_name = "cloudwatch-alarms-new"
+  sns_topic_name = "cloudwatch-alarms"
 
 }
