8a1a026e02fe0507cc4522ff9240592c7a426007
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-03-18 21:55:16 -0400

    refactor cloudtrail

--- terraform/modules/cloudtrail/cloudtrail.tf
+++ terraform/modules/cloudtrail/cloudtrail.tf
@@ -1,15 +1,26 @@
 #trivy:ignore:AVD-AWS-0015 (HIGH): CloudTrail does not use a customer managed key to encrypt the logs.
-resource "aws_cloudtrail" "multi_region_trail" {
+resource "aws_cloudtrail" "organization_trail" {
   name = local.cloudtrail_name
 
   enable_log_file_validation    = true
   enable_logging                = true
   include_global_service_events = true
   is_multi_region_trail         = true
+  is_organization_trail         = true
 
   s3_bucket_name = var.log_bucket_name
   s3_key_prefix  = local.cloudtrail_s3_key_prefix
 
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_to_cloudwatch_role.arn
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy.cloudtrail_to_cloudwatch_policy,
+    aws_cloudwatch_log_group.cloudtrail,
+  ]
 }
--- terraform/modules/cloudtrail/data.tf
+++ terraform/modules/cloudtrail/data.tf
@@ -1,5 +1,8 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_organizations_organization" "current" {}
+
 data "aws_region" "current" {}
 
 data "aws_regions" "enabled" {}
+
