87efbe2dedc326abf5121a8bcfdaffd171fe63b7
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-05-10 16:26:30 -0400

    adjust dump github context

--- scripts/dump_github_context.sh
+++ scripts/dump_github_context.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "Dumping context...\n"
+echo -e "Dumping github context...\n"
 
 echo "::group::env context (filtered):"
 echo "$CONTEXT_ENV" | grep -v -i "aws" # filter out any AWS creds from the env context
@@ -46,7 +46,4 @@ echo "::group::vars context:"
 echo "$CONTEXT_VARS"
 echo "::endgroup::"
 
-# if the ANNOTATE env var is set to "true", then annotate the context details in the GitHub Actions log
-if [[ "$ANNOTATE" = "true" ]]; then
-  echo -e "\n::notice::Context details.\n"
-fi
+echo -e "\n::notice::Context details.\n"
