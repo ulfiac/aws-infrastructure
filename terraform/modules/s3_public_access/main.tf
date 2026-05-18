b6ed4c48a0e3747a8690f4030470f320884f7fb2
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-05-17 21:07:10 -0400

    fix resource name

--- terraform/modules/s3_public_access/main.tf
+++ terraform/modules/s3_public_access/main.tf
@@ -1,6 +1,6 @@
 locals {}
 
-resource "aws_s3_account_public_access_block" "example" {
+resource "aws_s3_account_public_access_block" "block_public_access" {
   block_public_acls   = true
   block_public_policy = true
 }
