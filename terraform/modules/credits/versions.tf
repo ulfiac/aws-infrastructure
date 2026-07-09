5f5f802348692d780dd31bd354494a081723dfab
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-07-08 21:24:27 -0400

    remove renovate undefined

--- terraform/modules/credits/versions.tf
+++ terraform/modules/credits/versions.tf
@@ -1,4 +1,17 @@
 terraform {
-  required_providers {}
-  required_version = ">= 1.14.1" # current as of 2025-12-03
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.24.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.7.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.2"
+    }
+  }
+  required_version = ">= 1.14.1"
 }
