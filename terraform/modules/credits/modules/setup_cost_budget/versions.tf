e8cecb82c8d90791c9b762ac0d8e9c5b9d0d468f
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-08-01 13:50:09 -0400

    set some things to old versions to test renovate grouping pacakges

--- terraform/modules/credits/modules/setup_cost_budget/versions.tf
+++ terraform/modules/credits/modules/setup_cost_budget/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.57.1"
+      version = "6.55.0"
     }
   }
-  required_version = "1.15.8"
+  required_version = "1.15.7"
 }
--- terraform/modules/user/versions.tf
+++ terraform/modules/user/versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.57.1"
+      version = "6.55.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.9.0"
+      version = "2.8.0"
     }
   }
-  required_version = "1.15.8"
+  required_version = "1.15.7"
 }
